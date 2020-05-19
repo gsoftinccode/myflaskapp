5d214c07e1096d3589510644a856a0658bf1222e
gsoftinccode <47943806+gsoftinccode@users.noreply.github.com>
2020-05-19 23:33:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,10 @@ app= Flask(__name__)
 data= { "name": "Godwin",
          "age": 24,
          "gender":"Male"
-	
-		}
+	}
 @app.route('/home',methods=['POST'])
 def sendBack():
 
 	return jsonify(data)
-
-
-	
-
 if __name__=="__main__":
-	app.run()
+	app.run(host='0.0.0.0')
