15da7cbfa5c5eabf15af56750726335de5aa83e4
gsoftinccode <47943806+gsoftinccode@users.noreply.github.com>
2020-05-19 22:14:07 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import request, jsonify, Flask,json
+
+app= Flask(__name__)
+
+data= { "name": "Godwin",
+         "age": 24,
+         "gender":"Male"
+	
+		}
+@app.route('/home',methods=['POST'])
+def sendBack():
+
+	return jsonify(data)
+
+
+	
+
+if __name__=="__main__":
+	app.run(debug=True)
